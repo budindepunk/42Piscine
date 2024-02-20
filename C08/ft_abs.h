3bb64ce870069c9c0d0b95136734c8b318e3e61c
Carolina Silva Rode <carolinasilvarode@gmail.com>
2024-02-20 09:50:10 +0100

    08

---
+++ C08/ft_abs.h
@@ -0,0 +1,6 @@
+#ifndef ft_abs
+#define ft_abs
+
+#define ABS(n) ((n<0) ? (-n):(n))
+
+#endif
---
+++ C08/ft_boolean.h
@@ -0,0 +1,15 @@
+#ifndef ft_boolean
+#define ft_bollean
+
+#include "unistd.h"
+
+#define TRUE 1
+#define SUCCESS TRUE
+#define FALSE 0
+#define EVEN(nb) (NB % 2 == 0)
+#define EVEN_MSG "I have an even number of arguments"
+#define ODD_MSG "I have an odd number of arguments"
+
+typedef int	t_bool;
+
+#endif
